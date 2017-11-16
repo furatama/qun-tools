25719f6d6691e5790d432b8f04147ab54cd4bdf0
Bayu Fura <bayu.furatama@gmail.com>
2017-11-16 17:59:42 +0800

    update 2

--- index.js
+++ index.js
@@ -53,23 +53,28 @@ app.post('/webhook/', function (req, res) {
 	    let sender = event.sender.id
 	    if (event.message && event.message.text) {
 		    let text = event.message.text
-		    if (text === 'Belajar FBTools') {
+		    if (text === 'GraphQL') {
 		    	// sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-			    sendSecondA(sender)
+			    sendGraphQL(sender)
 		    	continue
 		    }
-		    if (text === 'Pertanyaan FBTools') {
-		    	// sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-			    sendSecondB(sender)
-		    	continue
-		    }
-		    if (text === 'Event Disekitar') {
-		    	// sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-			    sendSecondC(sender)
-		    	continue
-		    }
-		    sendFirst(sender)
-		    // sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+		    // if (text === 'Messenger Bot') {
+		    // 	// sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+			   //  sendMessengerBot(sender)
+		    // 	continue
+		    // }
+		    // if (text === 'React Native') {
+		    // 	// sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+			   //  sendReactNative(sender)
+		    // 	continue
+		    // }
+		    // if (text === 'React JS') {
+		    // 	// sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+			   //  sendReactJS(sender)
+		    // 	continue
+		    // }
+		    // sendFirst(sender)
+		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 	    }
     }
     res.sendStatus(200)
@@ -94,32 +99,34 @@ function sendTextMessage(sender, text) {
     })
 }
 
-function sendSecondC(sender) {
+function sendGraphQL(sender) {
     let messageData = {
-	    "attachment": {
-		    "type": "template",
-		    "payload": {
-				"template_type": "generic",
-			    "elements": [{
-						"title": "DevC Bali Hackday",
-				    "subtitle": "12 November 2017, 09:00 - 15:00 @Aula Suastika, Fak Teknologi dan Informasi Unud",
-				    "image_url": "https://spi.unud.ac.id/wp-content/uploads/2013/02/100_3720.jpg",
-				    "buttons": [{
-					    "type": "web_url",
-					    "url": "https://www.messenger.com",
-					    "title": "Lihat Selengkapnya"
-				    }],
-			    }, {
-				    "title": "DevC Bali Run4Life",
-				    "subtitle": "31 November 2017, 07:00 - 10:00 @Lapangan Renon",
-				    "image_url": "https://st2.depositphotos.com/3431221/9276/v/950/depositphotos_92765878-stock-illustration-run-vector-runner-abstract-silhouette.jpg",
-				    "buttons": [{
-					    "type": "web_url",
-					    "url": "https://www.messenger.com",
-					    "title": "Lihat Selengkapnya"
-				    }],
-			    }]
-		    }
+	    "attachment":{
+	      "type":"template",
+	      "payload": {
+	        "template_type":"generic",
+	        "elements":[
+	           {
+	            "title":"GraphQL",
+	            "image_url":"http://graphql.org/img/logo.svg",
+	            "buttons":[
+	              {
+	                "type":"postback",
+	                "title":"Send A Message",
+	                "payload":"MESSAGE_PAYLOAD"
+	              },{
+	                "type":"postback",
+	                "title":"Tutorials Collection",
+	                "payload":"TUTORIAL_PAYLOAD"
+	              },{
+	                "type":"postback",
+	                "title":"DevC Events Near You",
+	                "payload":"EVENT_PAYLOAD"
+	              }              
+	            ]      
+	          }
+	        ]
+	      }
 	    }
     }
     request({
@@ -138,163 +145,3 @@ function sendSecondC(sender) {
 	    }
     })
 }
-
-function sendFirst(sender) {
-    let messageData = {
-	    "attachment": {
-		    "type": "template",
-		    "payload": {
-					"template_type": "button",
-					"text": "Apa yang bisa dibantu?",
-			    "buttons":[
-			    	{
-			    		"type":"postback",
-			    		"title":"Belajar FBTools",
-			    		"payload":"Belajar FBTools"
-			    	},
-			    	{
-			    		"type":"postback",
-			    		"title":"Pertanyaan FBTools",
-			    		"payload":"Pertanyaan FBTools"
-			    	},
-			    	{
-			    		"type":"postback",
-			    		"title":"Event Disekitar",
-			    		"payload":"Event Disekitar"
-			    	}
-			    ]
-		    }
-	    }
-    }
-    request({
-	    url: 'https://graph.facebook.com/v2.6/me/messages',
-	    qs: {access_token:token},
-	    method: 'POST',
-	    json: {
-		    recipient: {id:sender},
-		    message: messageData,
-	    }
-    }, function(error, response, body) {
-	    if (error) {
-		    console.log('Error sending messages: ', error)
-	    } else if (response.body.error) {
-		    console.log('Error: ', response.body.error)
-	    }
-    })
-}
-
-function sendSecondA(sender) {
-    let messageData = {
-	    "attachment": {
-		    "type": "template",
-		    "payload": {
-					"template_type": "button",
-					"text": "Apa yang bisa dibantu?",
-			    "buttons":[
-			    	{
-			    		"type":"postback",
-			    		"title":"Belajar FBTools",
-			    		"payload":"Belajar FBTools"
-			    	},
-			    	{
-			    		"type":"postback",
-			    		"title":"Pertanyaan FBTools",
-			    		"payload":"Pertanyaan FBTools"
-			    	},
-			    	{
-			    		"type":"postback",
-			    		"title":"Event Disekitar",
-			    		"payload":"Event Disekitar"
-			    	}
-			    ]
-		    }
-	    }
-    }
-    request({
-	    url: 'https://graph.facebook.com/v2.6/me/messages',
-	    qs: {access_token:token},
-	    method: 'POST',
-	    json: {
-		    recipient: {id:sender},
-		    message: messageData,
-	    }
-    }, function(error, response, body) {
-	    if (error) {
-		    console.log('Error sending messages: ', error)
-	    } else if (response.body.error) {
-		    console.log('Error: ', response.body.error)
-	    }
-    })
-}
-
-// function sendSecondA(sender) {
-//     let messageData = {
-// 	    "attachment": {
-// 		    "type": "template",
-// 		    "payload": {
-// 					"template_type": "button",
-// 					"text": "Mau belajar apa?",
-// 			    "buttons":[
-// 			    	{
-// 			    		"type":"web_url",
-// 			    		"title":"Belajar React-Native",
-// 			    		"url":"https://facebook.github.io/react-native/docs/getting-started.html"
-// 			    	},
-// 			    	{
-// 			    		"type":"web_url",
-// 			    		"title":"Belajar React-JS",
-// 			    		"url":"https://reactjs.org/docs/hello-world.html"
-// 			    	},
-// 			    	{
-// 			    		"type":"web_url",
-// 			    		"title":"Belajar Messenger Bot",
-// 			    		"url":"https://messenger.fb.com/get-started"
-// 			    	},
-// 			    	{
-// 			    		"type":"web_url",
-// 			    		"title":"Belajar GraphQL",
-// 			    		"url":"http://graphql.org/learn/"
-// 			    	}
-// 			    ]
-// 		    }
-// 	    }
-//     }
-//     request({
-// 	    url: 'https://graph.facebook.com/v2.6/me/messages',
-// 	    qs: {access_token:token},
-// 	    method: 'POST',
-// 	    json: {
-// 		    recipient: {id:sender},
-// 		    message: messageData,
-// 	    }
-//     }, function(error, response, body) {
-// 	    if (error) {
-// 		    console.log('Error sending messages: ', error)
-// 	    } else if (response.body.error) {
-// 		    console.log('Error: ', response.body.error)
-// 	    }
-//     })
-// }
-
-function sendSecondB(sender) {
-    let messageData = { text:"Untuk bertanya silahkan menggunakan format \"Tanya<spasi><pertanyaan>\"" }
-    request({
-	    url: 'https://graph.facebook.com/v2.6/me/messages',
-	    qs: {access_token:token},
-	    method: 'POST',
-		json: {
-		    recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-		    console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-		    console.log('Error: ', response.body.error)
-	    }
-    })
-}
-
-//curl -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=<PAGE_ACCESS_TOKEN>"
-
-//EAAFM7BdQJXUBADpP8pqNt2ZAJ5ZAweXCujk4XPCsTsar1MPnofRhkwwPFWQmcxDrhBbDRmY3K4ZAw2n6hFixQUZAA70QUjEXjqWl1S8SJ2YxfrpLxeEKuKEJK7ZCHmAxcVZCvn3jhZCgQoaWxDHZBtTGVPGuSGpZBISx6lkutVGxHOQZDZD
